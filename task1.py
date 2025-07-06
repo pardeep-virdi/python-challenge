73d3d65acfd1c287a041ccf72caef55697b9c43c
pardeep-virdi <31764809+pardeep-virdi@users.noreply.github.com>
2025-07-06 23:43:24 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,18 +1,10 @@
-#factorial
+# Task 1: Read a File and Handle Errors
 
-def fact(number):
-    if number == 0:
-        return 1  # Factorial of 0 is 1
-    total = 1
-    for num in range(1, number + 1):
-          total *= num
-    return total
+filename = "sample.txt"
 
-if __name__ == "__main__":
-    number = int(input("Enter the number: "))
-    if number < 0:
-       print( number, "is Negative")
-       exit()
-    result = fact(number)
-    print("Factorial of", number, "is:", result )
-    
+try:
+    with open(filename, 'r') as file:
+        for n, line in enumerate(file, start=1):
+            print(f"line{n}: {line}", end='')  # end='' avoids adding extra newlines
+except FileNotFoundError:
+    print(f"Error: The file '{filename}' does not exist.")
