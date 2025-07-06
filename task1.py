e67f8206824f04807abbe431ae3454f56834a76a
pardeep-virdi <31764809+pardeep-virdi@users.noreply.github.com>
2025-07-06 14:15:51 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,13 +1,18 @@
+#factorial
 
-
-def check_even_odd(number):
-    if number % 2 == 0:
-        return f"{number} is an even number"
-    else:
-        return f"{number} is an odd number"
+def fact(number):
+    if number == 0:
+        return 1  # Factorial of 0 is 1
+    total = 1
+    for num in range(1, number + 1):
+          total *= num
+    return total
 
 if __name__ == "__main__":
     number = int(input("Enter the number: "))
-    result = check_even_odd(number)
-    print(result)
+    if number < 0:
+       print( number, "is Negative")
+       exit()
+    result = fact(number)
+    print("Factorial of", number, "is:", result )
     
